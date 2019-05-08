b8d95da8ea795205b0dd0808e7611f9e11d6c934
EmmanuelLopez1 <maximino33@hotmail.com>
2019-05-08 18:23:49 -0500

    Proyecto subido

---
+++ src/app/title/title.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-title',
+	templateUrl:'title.component.html',
+	styleUrls: ['./title.component.css']
+})
+
+export class TitleComponent implements OnInit{
+	public name : string;	
+	public edad : number;	
+	
+
+	constructor(){}
+
+	ngOnInit(){
+		this.name = 'Uriel';
+		this.edad = 24;
+
+	}
+
+	getEdadNombre() : string{
+		return `${this.name} : ${this.edad}`;
+	}
+}
+
